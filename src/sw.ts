1284cc94585f98658b283a158d77fc8e1c17ac3d
George Boozin <georgeboozin@boozyair.local>
2024-02-11 17:29:42 +0300

    add precache

--- src/sw.ts
+++ src/sw.ts
@@ -1,1 +1,6 @@
-console.log("service worker");
+import { precacheAndRoute } from "workbox-precaching";
+import type { PrecacheEntry } from "workbox-precaching";
+
+declare const PRECACHE_ENTRIES: Array<PrecacheEntry | string>;
+
+precacheAndRoute(PRECACHE_ENTRIES);
--- sw-build.mjs
+++ sw-build.mjs
@@ -1,7 +1,54 @@
 import * as esbuild from "esbuild";
+import fs from "fs";
+import path from "path";
+
+/**
+ * read recursive directory
+ * @param {string} dir - path to dir
+ * @param {string[]} fileList - list of files
+ * @return {string[]} fileList - list of files
+ * @example
+ *     readDir('public')
+ *     // ['public/manifest.json','public/favicon.ico', ...]
+ */
+async function readDir(dir, fileList = []) {
+  const files = await fs.promises.readdir(dir);
+  for (let file of files) {
+    const filePath = path.join(dir, file);
+    const stat = await fs.promises.stat(filePath);
+    if (stat.isDirectory()) {
+      fileList = await readDir(filePath, fileList);
+    } else {
+      fileList.push(filePath);
+    }
+  }
+  return fileList;
+}
+
+const staticDir = ".next/static";
+const publicDir = "public";
+const BUILD_ID = await fs.promises.readFile(".next/BUILD_ID", "utf8");
+
+const publicStaticPrecacheEntries = (await readDir(publicDir)).map((url) => ({
+  url: url.replace("public/", "/"),
+  revision: BUILD_ID,
+}));
+// [{ url: string; revision: string }]
+
+const nextStaticPrecacheEntries = (await readDir(staticDir)).map((url) => ({
+  url: url.replace(".next", "/_next"),
+  revision: BUILD_ID,
+}));
+
+const precacheEntries = []
+  .concat(publicStaticPrecacheEntries)
+  .concat(nextStaticPrecacheEntries);
 
 await esbuild.build({
   entryPoints: ["src/sw.ts"],
   bundle: true,
   outfile: "public/sw.js",
+  define: {
+    PRECACHE_ENTRIES: JSON.stringify(precacheEntries),
+  },
 });
